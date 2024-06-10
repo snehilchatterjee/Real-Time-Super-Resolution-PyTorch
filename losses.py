b77399bbe582721746debea1fdfe1489b4056861
Snehil Chatterjee <snehilchatterjee7924@gmail.com>
2024-06-10 14:08:55 +0530

    Adversial loss compeleted

--- losses.py
+++ losses.py
@@ -6,6 +6,7 @@ import torchvision.transforms as transforms
 
 class PixelLossTraining(nn.Module):
     def __init__(self, pixel_loss):
+        super().__init__()
         if pixel_loss == 'l1':
             self.pixel_loss_type = nn.L1Loss()
         elif pixel_loss == 'l2':
@@ -15,8 +16,9 @@ class PixelLossTraining(nn.Module):
         return self.pixel_loss_type(hrs, srs)
 
 
-class VGGContentTraining:
-    def setup_content_loss(self, content_loss):
+class VGGContentTraining(nn.Module):
+    def __init__(self, content_loss):
+        super().__init__()
         if content_loss == 'l1':
             self.content_loss_type = nn.L1Loss()
         elif content_loss == 'l2':
@@ -47,14 +49,13 @@ class VGGContentTraining:
         for param in self.feature_extractor.parameters():
             param.requires_grad = False
     
-    def content_loss(self, srs, hrs):
+    def __call__(self, srs, hrs):
         preprocess = transforms.Compose([
             transforms.Resize((224, 224))])
         
         srs = preprocess(srs)
         hrs = preprocess(hrs)
-        
-        
+          
         srs_features = self.feature_extractor(srs)
         val1 = self.outputs['layer8']
         val2= self.outputs['layer17']
@@ -73,3 +74,28 @@ class VGGContentTraining:
         
         return loss
     
+class AdverversialTraining(nn.Module):
+    def __init__(self, adv_loss):
+        super().__init__()
+        self.adv_loss_type = adv_loss
+        self.binary_cross_entropy = nn.BCEWithLogitsLoss()
+        
+    def gen_adv_loss(self, fake_logits, real_logits=None):
+        if self.adv_loss_type == 'gan':
+            return self.binary_cross_entropy(fake_logits, torch.ones_like(fake_logits))
+        
+        elif self.adv_loss_type == 'ragan':
+            real_loss = self.binary_cross_entropy(real_logits - torch.mean(fake_logits), torch.ones_like(real_logits))
+            fake_loss = self.binary_cross_entropy(fake_logits - torch.mean(real_logits), torch.zeros_like(fake_logits))
+            return (real_loss + fake_loss)
+    
+    def disc_adv_loss(self, fake_logits, real_logits):
+        if self.adv_loss_type == 'gan':
+            fake_loss = self.binary_cross_entropy(fake_logits, torch.zeros_like(fake_logits))
+            real_loss = self.binary_cross_entropy(real_logits, torch.ones_like(real_logits))
+            return (fake_loss + real_loss)
+        
+        elif self.adv_loss_type == 'ragan':
+            real_loss = self.binary_cross_entropy(real_logits - torch.mean(fake_logits), torch.ones_like(real_logits))
+            fake_loss = self.binary_cross_entropy(fake_logits - torch.mean(real_logits), torch.zeros_like(fake_logits))
+            return (real_loss + fake_loss)
